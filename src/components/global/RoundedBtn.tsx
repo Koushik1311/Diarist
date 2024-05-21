bb8e7763b4f6b92ca1bf79fd85a923fffa9809d6
Koushik1311 <programmer.koushik@gmail.com>
2024-05-21 11:14:11 +0530

    rounded btn updated

--- src/components/global/RoundedBtn.tsx
+++ src/components/global/RoundedBtn.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 type RoundedBtnProps = {
   children: React.ReactNode;
+  className: string;
 };
 
-export default function RoundedBtn({ children }: RoundedBtnProps) {
+export default function RoundedBtn({ children, className }: RoundedBtnProps) {
   return (
-    <button className="mt-5 inline-block text-sm font-bold bg-black rounded-full text-white py-3 px-8">
+    <button
+      className={`mt-5 inline-block text-base font-medium bg-slate-900 rounded-xl text-slate-50 h-10 px-5 ${className}`}
+    >
       {children}
     </button>
   );
