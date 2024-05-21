99b2aa2faa4c337c67a7f5964163158f3bb3d324
Koushik1311 <programmer.koushik@gmail.com>
2024-05-21 11:14:33 +0530

    landing page updated

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,42 +1,25 @@
-import Image from "next/image";
 import Link from "next/link";
-import { Playfair_Display } from "next/font/google";
 import RoundedBtn from "@/components/global/RoundedBtn";
-
-const playfair_display = Playfair_Display({ subsets: ["latin"] });
+import { IoPencilOutline } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <main className="max-w-[1250px] mx-auto lg:mx-4 xl:mx-auto">
-      <section className="flex flex-col lg:flex-row items-center text-center lg:text-left h-screen justify-center lg:h-auto lg:justify-between mx-4 lg:mx-0">
-        <div className="order-2 lg:order-1">
+    <main className="max-w-[1260px] mx-auto lg:mx-4 xl:mx-auto">
+      <section className="flex items-center text-center h-screen justify-center mx-4 lg:mx-0">
+        <div className="">
           {/* Left */}
-          <h1
-            className={`${playfair_display.className} text-4xl lg:text-4xl xl:text-6xl font-bold md:px-10 lg:px-0 xl:w-[700px]`}
-          >
-            Embark on a Journey with Your Personal{" "}
-            <span className="underline underline-offset-8 lg:underline-offset-[15px] decoration-[#e87755]">
-              Digital Diary.
-            </span>
+          <h1 className="text-slate-800 text-4xl lg:text-4xl xl:text-5xl font-semibold md:leading-[2.9rem] xl:leading-[4.813rem] md:px-10 lg:px-0 xl:w-[900px]">
+            Embark on a Journey with Your Personal Digital Diary.
           </h1>
-          <p className="text-sm font-bold text-gray-400 mt-7 lg:mx-0 lg:mt-10 mb-1">
+          <p className="text-base font-medium text-slate-500 mt-4 lg:mx-0 lg:mt-6 mb-4">
             Capture Moments, Reflect your Actions, and Grow from There.
           </p>
-          <RoundedBtn>Get Started Now</RoundedBtn>
-        </div>
-
-        <div className="order-1 lg:order-2">
-          {/* Right */}
-          {/* ATTRIBUTE: <a href="https://storyset.com/work">Work illustrations by Storyset</a> */}
-          <Image
-            src="./Notebook-amico.svg"
-            width={500}
-            height={500}
-            quality={100}
-            priority
-            alt="hero-image"
-            className="w-[580px] h-auto lg:h-[580px]"
-          />
+          <Link href="/auth" className="flex items-center justify-center">
+            <RoundedBtn className="flex items-center space-x-2">
+              <IoPencilOutline />
+              <span>Start Writing</span>
+            </RoundedBtn>
+          </Link>
         </div>
       </section>
       {/* <h1>Welcome to Diarist</h1>
