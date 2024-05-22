015ccb4b3db4fe18ee23fb0c44ea6d58d68a16ef
Koushik1311 <programmer.koushik@gmail.com>
2024-05-22 23:50:47 +0530

    updated: diaryReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 import itemsReducer from "./features/items-slice";
+import diariesReducer from "./features/diaries-slice";
 import monthResucer from "./features/month-slice";
 import yearReducer from "./features/year-slice";
 import bookmarkColorReducer from "./features/bookmark-slice";
+import diaryIdReducer, { diaryId } from "./features/diaryId-slice";
 
 export const store = configureStore({
-  reducer: { itemsReducer, monthResucer, yearReducer, bookmarkColorReducer },
+  reducer: {
+    itemsReducer,
+    diariesReducer,
+    monthResucer,
+    yearReducer,
+    bookmarkColorReducer,
+    diaryIdReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
