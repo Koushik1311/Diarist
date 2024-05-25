9d6e03842d7141cd3283411aa17f48678b668b79
Koushik <programmer.koushik@gmail.com>
2024-05-25 18:12:02 +0530

    function to get user

---
+++ data/User.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@/utils/supabase/server";
+
+export const getUser = async () => {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user;
+};
