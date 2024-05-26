5db227422cc69e1b673e054b2bc1516bf6f0915b
Koushik <programmer.koushik@gmail.com>
2024-05-26 18:31:44 +0530

    editor

---
+++ components/diary/Edditor.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Placeholder from "@tiptap/extension-placeholder";
+import { BubbleMenu, EditorContent, Extension, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { Bold, Italic, List } from "lucide-react";
+import React from "react";
+
+export default function Edditor() {
+  const CustomTabExtension = Extension.create({
+    addKeyboardShortcuts() {
+      return {
+        Tab: () => {
+          const { state, dispatch } = this.editor.view;
+          const { tr } = state;
+          tr.insertText("          ");
+          dispatch(tr);
+          return true;
+        },
+      };
+    },
+  });
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "Write something …",
+      }),
+      CustomTabExtension,
+    ],
+    editorProps: {
+      attributes: {
+        class: "min-h-[300px] border-none ring-0 focus:outline-none py-5",
+      },
+    },
+  });
+
+  return (
+    <>
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive("bold") ? "is-active" : ""}
+          >
+            <Bold />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive("italic") ? "is-active" : ""}
+          >
+            <Italic />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive("strike") ? "is-active" : ""}
+          >
+            strike
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive("bulletList") ? "is-active" : ""}
+          >
+            <List />
+          </button>
+        </BubbleMenu>
+      )}
+      <EditorContent editor={editor} className="flex-1 w-full" />
+    </>
+  );
+}
