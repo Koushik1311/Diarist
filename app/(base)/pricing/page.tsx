280bc76fe3cd826dfbeac1a63bacdbb8a8fc98b3
Koushik <programmer.koushik@gmail.com>
2024-05-30 22:19:33 +0530

    pricing page

---
+++ app/(base)/pricing/page.tsx
@@ -0,0 +1,10 @@
+import Price from "@/components/subscription/price";
+import React from "react";
+
+export default function Pricing() {
+  return (
+    <div className="flex items-center justify-center h-[90vh]">
+      <Price />
+    </div>
+  );
+}
