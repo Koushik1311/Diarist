c28f5f83ece665f2e3a9010dd7699c6a6a44cf80
Koushik <programmer.koushik@gmail.com>
2024-06-01 22:53:52 +0530

    updated login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,13 @@
-import Link from "next/link";
-import { headers } from "next/headers";
+import SocialAuthButton from "@/components/user/SocialAuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
+import { IoKey } from "react-icons/io5";
+import React from "react";
+import Link from "next/link";
+import Logo from "@/components/global/Logo";
+import AuthButton from "@/components/user/AuthButton";
 
 export default function Login({
   searchParams,
@@ -25,95 +30,100 @@ export default function Login({
       return redirect("/login?message=Could not authenticate user");
     }
 
-    return redirect("/protected");
-  };
-
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
+    return redirect("/diary");
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <main className="container flex items-center justify-center">
       <Link
         href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        className="fixed top-0 pt-3 left-0 pl-3 lg:pl-6 w-screen bg-white"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
+        <Logo />
+        <div className="border-b mt-3 -ml-6 border-slate-200" />
       </Link>
+      <div className="w-[320px] mt-24">
+        <p className="text-xl font-semibold mb-6 flex flex-col">
+          <span className="text-slate-700">Make it. Write it.</span>
+          <span className="text-purple-400">
+            Log in to your Diarist account
+          </span>
+        </p>
+        <form className="flex flex-col gap-1 w-full">
+          <div className="flex flex-col gap-2">
+            <SocialAuthButton>
+              <FcGoogle className="text-lg" />
+              <span>Continue with Google</span>
+            </SocialAuthButton>
+            <SocialAuthButton>
+              <FaApple className="text-lg" />
+              <span>Continue with Apple</span>
+            </SocialAuthButton>
+            <SocialAuthButton>
+              <IoKey className="text-lg" />
+              <span>Continue with SSO</span>
+            </SocialAuthButton>
+          </div>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
-    </div>
+          <label
+            htmlFor="email"
+            className="text-xs text-slate-500 font-medium mt-3"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email id"
+            required
+            className="h-9 px-3 text-sm rounded-[6px] border border-slate-200 focus:outline-purple-400"
+          />
+          <label
+            htmlFor="password"
+            className="text-xs text-slate-500 font-medium mt-3"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            required
+            className="h-9 px-3 text-sm rounded-[6px] border border-slate-200 focus:outline-purple-400"
+          />
+          <AuthButton
+            className="w-full h-9 rounded-[6px] bg-purple-500 hover:bg-fuchsia-500 transition-all duration-150 text-sm font-semibold text-white mt-6"
+            formAction={signIn}
+            pendingText="Signing Up..."
+          >
+            Log In
+          </AuthButton>
+        </form>
+
+        <p className="text-xs text-center mt-6">
+          Don&apos;t have an Account?{" "}
+          <Link href="/signup" className="text-purple-400 font-medium">
+            Sign Up Now
+          </Link>
+        </p>
+
+        <p className="text-xs text-center mt-12 mb-16">
+          By continuing, you acknowledge that you understand and agree to the{" "}
+          <Link
+            href="/terms&conditions"
+            className="text-purple-400 font-medium"
+          >
+            Terms & Conditions
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="/terms&conditions"
+            className="text-purple-400 font-medium"
+          >
+            Privacy Policy
+          </Link>
+        </p>
+      </div>
+    </main>
   );
 }
