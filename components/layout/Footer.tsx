9545aaad77372c8fe39e71146973b5d387fdfb11
Koushik <programmer.koushik@gmail.com>
2024-06-01 13:45:28 +0530

    added copyright to footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="container">
+      <div>{/* Links */}</div>
+      <div className="border-b border-slate-200" />
+      <p className="text-lg text-slate-500 my-8 text-center">
+        Copyright 2024 &copy; Diarist
+      </p>
+    </footer>
+  );
+}
