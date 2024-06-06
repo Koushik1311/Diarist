368ccd717789db6e366ffa2b5abc067fe95b565a
Koushik <programmer.koushik@gmail.com>
2024-06-06 19:12:27 +0530

    months constants

---
+++ constants/months.ts
@@ -0,0 +1,50 @@
+export const Months = [
+  {
+    name: "Jan",
+    index: 1,
+  },
+  {
+    name: "Feb",
+    index: 2,
+  },
+  {
+    name: "Mar",
+    index: 3,
+  },
+  {
+    name: "Apr",
+    index: 4,
+  },
+  {
+    name: "May",
+    index: 5,
+  },
+  {
+    name: "Jun",
+    index: 6,
+  },
+  {
+    name: "Jul",
+    index: 7,
+  },
+  {
+    name: "Aug",
+    index: 8,
+  },
+  {
+    name: "Sep",
+    index: 9,
+  },
+  {
+    name: "Oct",
+    index: 10,
+  },
+  {
+    name: "Nov",
+    index: 11,
+  },
+  {
+    name: "Dec",
+    index: 12,
+  },
+] as const;
