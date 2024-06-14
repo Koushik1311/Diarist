d4b784fb8a447ba18867018c782d8693b28b9f7d
Koushik <programmer.koushik@gmail.com>
2024-06-14 22:23:02 +0530

    addentry button using new client component and new insert method

--- components/global/AddEntryButton.tsx
+++ components/global/AddEntryButton.tsx
@@ -1,7 +1,10 @@
-import { insertRecord } from "@/data/action/diary_entry";
+"use client";
+
+import { insertRecord } from "@/data/diary";
 import { getLocalYear } from "@/utils/local-day";
-import { redirect } from "next/navigation";
-import React, { useEffect } from "react";
+import { redirect, useRouter } from "next/navigation";
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 type Props = {
   children: React.ReactNode;
@@ -9,33 +12,41 @@ type Props = {
   userId: string;
 };
 
-export default async function AddEntryButton({
-  children,
-  className,
-  userId,
-}: Props) {
-  const insertEntry = async () => {
-    "use server";
-    const records = await insertRecord(userId);
-    if (records && records.length > 0) {
-      const lastRecord = records[records.length - 1];
-      redirect(`/diary/${getLocalYear()}/${lastRecord.id}`);
+export default function AddEntryButton({ children, className, userId }: Props) {
+  const router = useRouter();
+
+  const insertEntry = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    try {
+      const { data, error } = await insertRecord(userId);
+      if (error) {
+        console.error("Error inserting record: ", error);
+        toast(error, {
+          icon: "💰",
+        });
+        // Handle error scenario, e.g., show a message to the user
+      }
+
+      console.log(data);
+
+      if (data) {
+        const lastRecord = data[data.length - 1];
+        router.push(`/diary/${getLocalYear()}/${lastRecord.id}`);
+        toast.success("Entry created");
+      }
+    } catch (error) {
+      console.error("Error inserting record: ", error);
+      // Handle unexpected error scenario
     }
   };
 
-  // useEffect(() => {
-  //   const insertEntry = async () => {
-  //     "use server"
-  //     const record = await insertRecord();
-  //     if (record) {
-  //       console.log()
-  //     }
-  //   }
-  // })
-
   return (
-    <form action={insertEntry}>
-      <button className={className}>{children}</button>
+    <form>
+      <button className={className} onClick={insertEntry}>
+        {children}
+      </button>
+      <Toaster position="top-right" />
     </form>
   );
 }
