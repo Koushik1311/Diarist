d7bd82671e4afa1b7e7e719eaa28f019852f7b12
Koushik <programmer.koushik@gmail.com>
2024-06-14 23:48:41 +0530

    style toggle buttons

--- app/(base)/pricing/page.tsx
+++ app/(base)/pricing/page.tsx
@@ -1,5 +1,7 @@
 import Bundle from "@/components/subscription/Bundle";
 import LifetimeAccess from "@/components/subscription/LifetimeAccess";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { notFound, redirect } from "next/navigation";
 import React from "react";
 
@@ -23,8 +25,28 @@ export default function Price({
   return (
     <div>
       <form className="flex items-center justify-center gap-5">
-        <button formAction={bundleClick}>Bundle</button>
-        <button formAction={lifetimeClick}>Lifetime</button>
+        <Button
+          formAction={bundleClick}
+          className={cn(
+            "hover:bg-purple-500 text-white transition-colors",
+            searchParams.pricing === "bundle" || !searchParams.pricing
+              ? "bg-purple-500"
+              : "bg-white text-black"
+          )}
+        >
+          Bundle
+        </Button>
+        <Button
+          formAction={lifetimeClick}
+          className={cn(
+            "hover:bg-purple-500 text-white transition-colors",
+            searchParams.pricing === "lifetime"
+              ? "bg-purple-500"
+              : "bg-white text-black"
+          )}
+        >
+          Lifetime
+        </Button>
       </form>
       <div className="flex items-center justify-center h-[90vh]">
         {searchParams.pricing === "bundle" || !searchParams.pricing ? (
