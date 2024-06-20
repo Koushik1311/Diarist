5c084d34ff918c3f41703f144bad089e0bfc3ff3
Koushik <programmer.koushik@gmail.com>
2024-06-20 22:06:50 +0530

    get pricing products

---
+++ data/lemonsqueezy/getProducts.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const lsqyConfig = {
+  API_KEY: process.env.LEMONSQUEEZY_API_KEY,
+  URL: "https://api.lemonsqueezy.com/v1",
+};
+
+const headers = {
+  Accept: "application/vnd.api+json",
+  "Content-Type": "application/vnd.api+json",
+  Authorization: `Bearer ${lsqyConfig.API_KEY}`,
+};
+
+const getProducts = async () => {
+  try {
+    const response = await axios.get(`${lsqyConfig.URL}/products`, {
+      headers,
+    });
+
+    // Access the data array from the response
+    const products = response.data.data;
+
+    // Loop through the data array and log each product's attributes
+    // products.forEach((product) => {
+    //   console.log(product.attributes);
+    // });
+
+    return products;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getProducts };
