0ca04189e620f66fefa38df493ad7a716c45fe63
Koushik <programmer.koushik@gmail.com>
2024-06-22 20:03:58 +0530

    lifetime page

---
+++ app/(base)/pricing/lifetime/page.tsx
@@ -0,0 +1,10 @@
+import LifetimeAccess from "@/components/subscription/LifetimeAccess";
+import React from "react";
+
+export default function LifetimePricingPage() {
+  return (
+    <div className="flex items-center justify-center lg:h-[90vh]">
+      <LifetimeAccess />
+    </div>
+  );
+}
