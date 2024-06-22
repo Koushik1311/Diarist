a625e5a16e7a8aa09ca5b16e103189b5c3336330
Koushik <programmer.koushik@gmail.com>
2024-06-22 20:03:47 +0530

    bundle page

---
+++ app/(base)/pricing/bundle/page.tsx
@@ -0,0 +1,10 @@
+import BundlePricing from "@/components/subscription/BundlePricing";
+import React from "react";
+
+export default function BundlePricingPage() {
+  return (
+    <div className="flex items-center justify-center lg:h-[90vh]">
+      <BundlePricing />
+    </div>
+  );
+}
