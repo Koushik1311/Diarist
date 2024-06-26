0d9d1ccf4ad00a22f500644b7f9d0ad109e2bf30
Koushik <programmer.koushik@gmail.com>
2024-06-26 23:58:43 +0530

    pass prop isUser

--- app/(base)/pricing/lifetime/page.tsx
+++ app/(base)/pricing/lifetime/page.tsx
@@ -1,10 +1,14 @@
 import LifetimeAccess from "@/components/subscription/LifetimeAccess";
+import { getUser } from "@/data/User";
 import React from "react";
 
-export default function LifetimePricingPage() {
+export default async function LifetimePricingPage() {
+  const user = await getUser();
+  const isUser = !!user;
+
   return (
     <div className="flex items-center justify-center lg:h-[90vh]">
-      <LifetimeAccess />
+      <LifetimeAccess isUser={isUser} />
     </div>
   );
 }
