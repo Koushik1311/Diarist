cf9c6628ae0bdb6756a9ad108ca00411a2901d47
Koushik <programmer.koushik@gmail.com>
2024-06-26 23:53:50 +0530

    checkout using lemonsqueezy.js

---
+++ actions/lsCheckout.ts
@@ -0,0 +1,47 @@
+import { getUser } from "@/data/User";
+import { configureLemonSqueezy } from "@/utils/lemonsqueezy";
+import { createCheckout } from "@lemonsqueezy/lemonsqueezy.js";
+
+export async function getCheckoutURL(variantId: number, embed = true) {
+  configureLemonSqueezy();
+
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("User is not authenticated.");
+  }
+
+  console.log("Product Id: ", variantId);
+
+  const checkout = await createCheckout(
+    process.env.LEMONSQUEEZY_STORE_ID!,
+    variantId,
+    {
+      checkoutOptions: {
+        embed,
+        media: false,
+        logo: !embed,
+      },
+      checkoutData: {
+        email: user.email ?? undefined,
+        name: user?.user_metadata.display_name || user?.user_metadata.full_name,
+        custom: {
+          user_id: user.id,
+          name:
+            user?.user_metadata.display_name || user?.user_metadata.full_name,
+          email: user.email,
+        },
+      },
+      productOptions: {
+        enabledVariants: [variantId],
+        redirectUrl: `${process.env.NEXT_PUBLIC_APP_URL}/billing/`,
+        // receiptButtonText: "Go to Dashboard",
+        // receiptThankYouNote: "Thank you for signing up to Lemon Stand!",
+      },
+    }
+  );
+
+  console.log(checkout.data?.data);
+
+  //   return checkout.data?.data.attributes.url;
+}
