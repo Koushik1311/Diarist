ebc9a4eeb2e4ec9dc201ae090c93eb4113aa0806
Koushik <programmer.koushik@gmail.com>
2024-07-02 23:46:59 +0530

    added small device nav bar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -5,6 +5,8 @@ import GetStartedBtn from "../global/GetStartedBtn";
 import Logo from "../global/Logo";
 import { getUser } from "@/data/User";
 import { Button } from "../ui/button";
+import { Menu } from "lucide-react";
+import SmallDeviceNavBar from "./SmallDeviceNavBar";
 
 export default async function Navbar() {
   const user = await getUser();
@@ -12,16 +14,21 @@ export default async function Navbar() {
   return (
     <div className="container flex items-center justify-between mt-3">
       {/* <h1 className="font-bold text-2xl">LOGO</h1> */}
-      <Link href="/">
-        <Logo />
-      </Link>
+      <div className="flex items-center justify-center gap-2 lg:gap-0">
+        <div className="lg:hidden">
+          <SmallDeviceNavBar />
+        </div>
+        <Link href="/">
+          <Logo />
+        </Link>
+      </div>
       <div className="flex items-center gap-10">
         <div className="lg:flex items-center gap-8 hidden">
           {Navlinks.map((link, index) => (
             <Link
               href={link.link}
               key={index}
-              className="hover:text-purple-500 transition-colors duration-150"
+              className="hover:text-violet-500 transition-colors duration-150"
             >
               {link.name}
             </Link>
