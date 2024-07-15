031d495baf7877488bf29e5340d3654fa24421f6
Koushik <programmer.koushik@gmail.com>
2024-07-15 20:09:09 +0530

    if there is a subscription model user can't access onboarding page

--- app/onboarding/layout.tsx
+++ app/onboarding/layout.tsx
@@ -1,10 +1,25 @@
 import SubLinks from "@/components/subscription/SubLinks";
+import { getUser } from "@/data/User";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function OnboardingLayout({
+export default async function OnboardingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createClient();
+  const user = await getUser();
+  const { data } = await supabase
+    .from("subscriptions")
+    .select("id")
+    .eq("user_id", user?.id)
+    .single();
+
+  if (data) {
+    return redirect("/diary");
+  }
+
   return <main className="container">{children}</main>;
 }
