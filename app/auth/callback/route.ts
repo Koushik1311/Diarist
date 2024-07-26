505a2df07ad7fd25725b012bc6dd82d310202752
Koushik <programmer.koushik@gmail.com>
2024-07-26 14:49:25 +0530

    if there is no code verifier ther redirect to login page

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,10 +1,20 @@
 import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const cookieStore = cookies();
+
+  const authTokenCodeVerifier = cookieStore.get(
+    "sb-cytvltfddmnptpqimndl-auth-token-code-verifier"
+  );
+
+  if (!authTokenCodeVerifier) {
+    return NextResponse.redirect(`${requestUrl.origin}/login`);
+  }
 
   if (code) {
     const supabase = createClient();
@@ -12,12 +22,8 @@ export async function GET(request: NextRequest) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (error) {
-      console.error("Error during code exchange:", error);
-      console.error("Error during code exchange:", error.cause);
       return NextResponse.redirect(`${requestUrl.origin}/login`);
     }
-  } else {
-    console.warn("No code found in request URL");
   }
 
   return NextResponse.redirect(`${requestUrl.origin}/diary`);
