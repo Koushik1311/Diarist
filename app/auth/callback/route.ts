97c79e8784bcb95e42ad1ef76c9daac951d60ad2
Koushik <programmer.koushik@gmail.com>
2024-07-26 14:12:11 +0530

    redirect fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,19 +1,20 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const origin = requestUrl.origin;
 
   if (code) {
     const supabase = createClient();
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (error) {
-      return NextResponse.redirect(`${origin}/login`);
+      return NextResponse.redirect(`${requestUrl.origin}/login`);
     }
   }
 
-  return NextResponse.redirect(`${origin}/diary`);
+  return NextResponse.redirect(`${requestUrl.origin}/diary`);
 }
