6e2db6b28eaceb9d29c79e90a313ab0b60659cef
Koushik <programmer.koushik@gmail.com>
2024-07-27 11:57:45 +0530

    30 motivational quotes

---
+++ constants/motivationalQuotes.ts
@@ -0,0 +1,136 @@
+export const motivationalQuotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote:
+      "Success is not the key to happiness. Happiness is the key to success.",
+    author: "Albert Schweitzer",
+  },
+  {
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    quote:
+      "The harder you work for something, the greater you'll feel when you achieve it.",
+    author: "Unknown",
+  },
+  {
+    quote: "Dream bigger. Do bigger.",
+    author: "Unknown",
+  },
+  {
+    quote: "Don't stop when you're tired. Stop when you're done.",
+    author: "Unknown",
+  },
+  {
+    quote: "Wake up with determination. Go to bed with satisfaction.",
+    author: "Unknown",
+  },
+  {
+    quote: "Little things make big days.",
+    author: "Unknown",
+  },
+  {
+    quote: "It's going to be hard, but hard does not mean impossible.",
+    author: "Unknown",
+  },
+  {
+    quote: "Don't wait for opportunity. Create it.",
+    author: "Unknown",
+  },
+  {
+    quote:
+      "Sometimes we're tested not to show our weaknesses, but to discover our strengths.",
+    author: "Unknown",
+  },
+  {
+    quote: "The key to success is to focus on goals, not obstacles.",
+    author: "Unknown",
+  },
+  {
+    quote: "Dream it. Wish it. Do it.",
+    author: "Unknown",
+  },
+  {
+    quote: "Success doesn't just find you. You have to go out and get it.",
+    author: "Unknown",
+  },
+  {
+    quote: "The harder you work, the luckier you get.",
+    author: "Unknown",
+  },
+  {
+    quote: "Success is what happens after you have survived all your mistakes.",
+    author: "Unknown",
+  },
+  {
+    quote: "Do something today that your future self will thank you for.",
+    author: "Unknown",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote:
+      "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    author: "Roy T. Bennett",
+  },
+  {
+    quote: "Work hard in silence, let your success be your noise.",
+    author: "Frank Ocean",
+  },
+  {
+    quote:
+      "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Christian D. Larson",
+  },
+  {
+    quote:
+      "You don't have to be great to start, but you have to start to be great.",
+    author: "Zig Ziglar",
+  },
+  {
+    quote:
+      "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
+  },
+  {
+    quote: "Opportunities don't happen. You create them.",
+    author: "Chris Grosser",
+  },
+  {
+    quote: "Don't let yesterday take up too much of today.",
+    author: "Will Rogers",
+  },
+  {
+    quote:
+      "Failure will never overtake me if my determination to succeed is strong enough.",
+    author: "Og Mandino",
+  },
+  {
+    quote: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis",
+  },
+];
