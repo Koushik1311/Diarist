d3729403bc2e9da8721a404d326b06da1a18d7dc
Koushik <programmer.koushik@gmail.com>
2024-07-27 09:56:56 +0530

    metadeta

--- app/(base)/pricing/layout.tsx
+++ app/(base)/pricing/layout.tsx
@@ -1,5 +1,12 @@
 import SubLinks from "@/components/subscription/SubLinks";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Diarist - Affordable Plans for Your Personal Digital Diary",
+  description:
+    "Explore Diarist's pricing plans. Choose the best plan that suits your needs and start your journey with our digital diary today.",
+};
 
 export default function PricingLayout({
   children,
