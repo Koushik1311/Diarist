64f1a8e807ff3b6cea32921c632782c7ac51891f
Koushik <programmer.koushik@gmail.com>
2024-07-30 13:28:46 +0530

    added Moods

--- app/diary/[year]/[id]/page.tsx
+++ app/diary/[year]/[id]/page.tsx
@@ -7,6 +7,7 @@ import { getDay, getWeekday } from "@/utils/local-date-&-time";
 import TitleInput from "@/components/diary/TitleInput";
 import { NotebookPen } from "lucide-react";
 import OptionBtn from "@/components/diary/OptionBtn";
+import Moods from "@/components/diary/Moods";
 
 const dancing_script = Dancing_Script({ subsets: ["latin"] });
 
@@ -32,16 +33,18 @@ export default async function DiaryPage({
       </div>
 
       <div className="flex-1 overflow-y-scroll pt-20 pb-20 md:py-[20vh]">
-        {/* Editor */}
         <div className="max-w-2xl mx-3 md:mx-auto">
-          <div>
+          <div className="">
             <h1
-              className={`${dancing_script.className} text-4xl font-extrabold`}
+              className={`${dancing_script.className} text-4xl font-extrabold mb-2`}
             >
-              <span>{getWeekday(record.created_at)} </span>{" "}
+              <span>{getWeekday(record.created_at)} </span>
               <span>{getDay(record.created_at)}</span>
             </h1>
+            {/* Moods */}
+            <Moods id={id} />
           </div>
+          {/* Editor */}
           <div className="border-b border-zinc-200 mt-3" />
 
           <Editor id={id} content={record.content} />
