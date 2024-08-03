bd350e4a202c3fd723347124ea5790387f19c7bc
Koushik <programmer.koushik@gmail.com>
2024-08-03 13:20:56 +0530

    fix

--- app/diary/[year]/[id]/page.tsx
+++ app/diary/[year]/[id]/page.tsx
@@ -6,8 +6,8 @@ import { getSingleRecord } from "@/data/action/diary_entry";
 import { getDay, getWeekday } from "@/utils/local-date-&-time";
 import TitleInput from "@/components/diary/TitleInput";
 import { NotebookPen } from "lucide-react";
-import OptionBtn from "@/components/diary/OptionBtn";
 import Moods from "@/components/diary/Moods";
+import OptionBtn from "@/components/diary/OptionBtn";
 
 const dancing_script = Dancing_Script({ subsets: ["latin"] });
 
@@ -20,30 +20,26 @@ export default async function DiaryPage({
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex justify-between pl-10 md:px-6 py-2">
+      <div className="flex justify-between items-start pl-10 md:px-6 py-2">
         <div className="flex items-start">
           <div className="w-4 h-4">
             <NotebookPen className="w-4 h-4 mr-1 mt-1" />
           </div>
           <TitleInput id={id} title={record.title} />
         </div>
-        <div className="">
-          <OptionBtn id={id} />
-        </div>
+        <OptionBtn id={id} />
       </div>
 
       <div className="flex-1 overflow-y-scroll pt-20 pb-20 md:py-[20vh]">
         <div className="max-w-2xl mx-3 md:mx-auto">
-          <div className="">
-            <h1
-              className={`${dancing_script.className} text-4xl font-extrabold mb-2`}
-            >
-              <span>{getWeekday(record.created_at)} </span>
-              <span>{getDay(record.created_at)}</span>
-            </h1>
-            {/* Moods */}
-            <Moods id={id} />
-          </div>
+          <h1
+            className={`${dancing_script.className} text-4xl font-extrabold mb-2`}
+          >
+            <span>{getWeekday(record.created_at)} </span>
+            <span>{getDay(record.created_at)}</span>
+          </h1>
+          {/* Moods */}
+          <Moods id={id} />
           {/* Editor */}
           <div className="border-b border-zinc-200 mt-3" />
 
