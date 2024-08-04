8b803865b420d317b80c8dce8b7d8d13a863a24a
Koushik <programmer.koushik@gmail.com>
2024-08-04 17:38:55 +0530

    global database type declaration: DiaryEntry & SubscriptionType

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+import { Database as DB } from "./types_db";
+
+declare global {
+  type Database = DB;
+  type DiaryEntry = DB["public"]["Tables"]["diary_entries"]["Row"];
+  type SubscriptionType = DB["public"]["Tables"]["subscriptions"]["Row"];
+}
