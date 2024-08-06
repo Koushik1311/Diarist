fe3cdc78aae026d53caa8e6028a35998fcf42212
Koushik <programmer.koushik@gmail.com>
2024-08-06 09:48:58 +0530

    entry and time capsule creation helper function

---
+++ helper/entry-creation.ts
@@ -0,0 +1,70 @@
+import { fetchTodaysEntry, insertEntry } from "@/data/diary";
+import {
+  fetchSubscription,
+  updateSubscriptionData,
+} from "@/data/client/subscription";
+import { fetchTodaysTimeCapsuleEntry } from "@/data/timeCapsule";
+
+export const createEntry = async (userId: string) => {
+  const { subData, subError } = await fetchSubscription(userId);
+
+  if (subError) {
+    return { error: "Error inserting entry." };
+  }
+
+  if (subData?.lifetime !== "none") {
+    const entryLimit = subData?.daily_entry_limit;
+
+    // get how many entries have been created today
+    const { entriesToday, entriesError } = await fetchTodaysEntry();
+    const { todaysTimeCapsuleEntry, todaysTimeCapsuleEntryError } =
+      await fetchTodaysTimeCapsuleEntry();
+
+    if (entriesError || todaysTimeCapsuleEntryError) {
+      return { error: "Error inserting entry." };
+    }
+
+    const entriesCount = entriesToday ? entriesToday.length : 0;
+    const timeCapsuleEntryCount = todaysTimeCapsuleEntry
+      ? todaysTimeCapsuleEntry.length
+      : 0;
+
+    if (entryLimit! >= entriesCount || entryLimit >= timeCapsuleEntryCount) {
+      const { entryData, entryError } = await insertEntry(userId);
+
+      if (entryError) {
+        return {
+          error: "Error inserting entry.",
+        };
+      }
+
+      return { entryData };
+    }
+    return {
+      error:
+        "You have reached daily entry limit. Please purchase additional entries if needed.",
+    };
+  }
+
+  if (subData?.entries! > 0) {
+    // let the user insert
+
+    const { updateError } = await updateSubscriptionData({
+      userId: userId,
+      entries: subData.entries - 1,
+    });
+
+    if (updateError) {
+      return { error: "Error inserting entry." };
+    }
+
+    const { entryData, entryError } = await insertEntry(userId);
+
+    if (entryError) {
+      return { error: "Error inserting entry." };
+    }
+
+    return { entryData };
+  }
+  return { error: "You have no entries left. Please purchase more." };
+};
---
+++ helper/time-capsule-creation.ts
@@ -0,0 +1,75 @@
+import {
+  fetchSubscription,
+  updateSubscriptionData,
+} from "@/data/client/subscription";
+import { fetchTodaysEntry } from "@/data/diary";
+import {
+  fetchTodaysTimeCapsuleEntry,
+  insertTimeCapsuleEntry,
+} from "@/data/timeCapsule";
+
+export const createTimeCapsule = async (userId: string) => {
+  const { subData, subError } = await fetchSubscription(userId);
+
+  if (subError) {
+    return { error: "Error inserting Time Capsule." };
+  }
+
+  if (subData?.lifetime !== "none") {
+    const entryLimit = subData?.daily_entry_limit;
+
+    // get how many entries have been created today
+    const { entriesToday, entriesError } = await fetchTodaysEntry();
+    const { todaysTimeCapsuleEntry, todaysTimeCapsuleEntryError } =
+      await fetchTodaysTimeCapsuleEntry();
+
+    if (entriesError || todaysTimeCapsuleEntryError) {
+      return { error: "Error inserting Time Capsule." };
+    }
+
+    const entriesCount = entriesToday ? entriesToday.length : 0;
+    const timeCapsuleEntryCount = todaysTimeCapsuleEntry
+      ? todaysTimeCapsuleEntry.length
+      : 0;
+
+    if (entryLimit! >= entriesCount || entryLimit >= timeCapsuleEntryCount) {
+      const { timeCapsuleEntry, timeCapsuleEntryError } =
+        await insertTimeCapsuleEntry(userId);
+
+      if (timeCapsuleEntryError) {
+        return {
+          error: "Error inserting Time Capsule.",
+        };
+      }
+
+      return { timeCapsuleEntry };
+    }
+    return {
+      error:
+        "You have reached daily entry limit. Please purchase additional entries if needed.",
+    };
+  }
+
+  if (subData?.entries! > 0) {
+    // let the user insert
+
+    const { updateError } = await updateSubscriptionData({
+      userId: userId,
+      entries: subData.entries - 1,
+    });
+
+    if (updateError) {
+      return { error: "Error inserting Time Capsule." };
+    }
+
+    const { timeCapsuleEntry, timeCapsuleEntryError } =
+      await insertTimeCapsuleEntry(userId);
+
+    if (timeCapsuleEntryError) {
+      return { error: "Error inserting Time Capsule." };
+    }
+
+    return { timeCapsuleEntry };
+  }
+  return { error: "You have no entries left. Please purchase more." };
+};
