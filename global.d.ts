5400eb38d655bfc7fb56dd5d66f5c5bfd47fb768
Koushik <programmer.koushik@gmail.com>
2024-08-06 09:43:57 +0530

    added TimeCapsule

--- global.d.ts
+++ global.d.ts
@@ -4,4 +4,5 @@ declare global {
   type Database = DB;
   type DiaryEntry = DB["public"]["Tables"]["diary_entries"]["Row"];
   type SubscriptionType = DB["public"]["Tables"]["subscriptions"]["Row"];
+  type TimeCapsule = DB["public"]["Tables"]["time_capsules"]["Row"];
 }
