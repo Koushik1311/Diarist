37140b399ab8ae991816819ced6f7b3e8047ebf9
Koushik <programmer.koushik@gmail.com>
2024-08-07 13:24:53 +0530

    added: MoodType, MoodType, TimeCapsulseMoodsType

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,12 @@ import { Database as DB } from "./types_db";
 
 declare global {
   type Database = DB;
-  type DiaryEntry = DB["public"]["Tables"]["diary_entries"]["Row"];
+  type DiaryEntryType = DB["public"]["Tables"]["diary_entries"]["Row"];
   type SubscriptionType = DB["public"]["Tables"]["subscriptions"]["Row"];
-  type TimeCapsule = DB["public"]["Tables"]["time_capsules"]["Row"];
+  type TimeCapsuleType = DB["public"]["Tables"]["time_capsules"]["Row"];
+  type MoodType = DB["public"]["Tables"]["moods"]["Row"];
+  type DiaryEntriesMoodsType =
+    DB["public"]["Tables"]["diary_entries_moods"]["Row"];
+  type TimeCapsulseMoodsType =
+    DB["public"]["Tables"]["time_capsules_moods"]["Row"];
 }
