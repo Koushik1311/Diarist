f5f42c3b489ac4c00e366491fb34ec3b5682706b
Koushik <programmer.koushik@gmail.com>
2024-08-10 11:54:32 +0530

    fetch mood id

--- data/client/time-capsule-mood.ts
+++ data/client/time-capsule-mood.ts
@@ -15,3 +15,19 @@ export const insertTimeCapsuleMood = async (
     return { error: error.message };
   }
 };
+
+export const fetchMoodId = async (timeCapsuleId: string) => {
+  const supabase = browserClient();
+
+  const { data, error } = await supabase
+    .from("time_capsules_moods")
+    .select("mood_id")
+    .eq("time_capsule_id", timeCapsuleId);
+
+  if (error) {
+    console.error("Error getting mood_id: ", error.message);
+    return { error };
+  }
+
+  return { data };
+};
