2fbb9f3d94349dcbbde5afc9634fbaa4de19f8d0
Koushik <programmer.koushik@gmail.com>
2024-08-23 21:43:21 +0530

    added motivational quots

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -5,8 +5,28 @@ import { Sacramento } from "next/font/google";
 import { getLocalYear } from "@/utils/local-day";
 import Link from "next/link";
 import { Key, Lock } from "lucide-react";
+import { motivationalQuotes } from "@/constants/motivationalQuotes";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { bebas_neus } from "@/utils/google-fonts";
 
 const sacramento = Sacramento({ subsets: ["latin"], weight: "400" });
+
+// Function to get the current day of the year
+const getDayOfYear = (date: Date): number => {
+  const start = new Date(date.getFullYear(), 0, 0);
+  const diff = date.getTime() - start.getTime();
+  const oneDay = 1000 * 60 * 60 * 24;
+  const dayOfYear = Math.floor(diff / oneDay);
+  return dayOfYear;
+};
+
+// Function to get the quote for the current day
+const getQuoteForToday = () => {
+  const dayOfYear = getDayOfYear(new Date());
+  // Use the modulo operator to cycle through the quotes array
+  const quoteIndex = dayOfYear % motivationalQuotes.length;
+  return motivationalQuotes[quoteIndex];
+};
 
 export default async function DiaryHome() {
   const user = await getUser();
@@ -15,30 +35,45 @@ export default async function DiaryHome() {
     return redirect("/login");
   }
 
+  const { quote, author } = getQuoteForToday();
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <h1 className={`${sacramento.className} text-[10rem]`}>Diary</h1>
-        <div className="flex flex-col items-center">
-          <p className="text-2xl uppercase tracking-[0.5em] text-center">
-            {user?.user_metadata.display_name || user?.user_metadata.full_name}
-          </p>
-          <button className="text-2xl uppercase tracking-[0.5em] text-center mt-2">
+      <div className="flex flex-col items-center gap-6">
+        <div
+          className={` max-w-xl pt-6 flex flex-col items-center justify-center text-5xl`}
+        >
+          {/* <RiDoubleQuotesL className="w-12 h-12 text-gray-600" /> */}
+          <button
+            className={`${bebas_neus.className} ml-5 text-3xl text-zinc-600 uppercase tracking-[0.5em] text-center mt-2`}
+          >
             {getLocalYear()}
           </button>
-
-          <Link
-            href={`/diary/${getLocalYear()}`}
-            className="flex items-center group mt-5"
+          <p
+            className={`${sacramento.className} text-zinc-600 text-center mt-5`}
           >
-            <Lock className="w-[18px] h-[18px] group-hover:text-white group-hover:-translate-x-3 mr-2 transition duration-500" />
-            <span className="uppercase text-lg tracking-[0.4em] last:tracking-[0px]">
-              Open
-            </span>
-            <Key className="w-[18px] h-[18px] ml-[1px] translate-x-3 text-white group-hover:text-zinc-950 group-hover:translate-x-0 transition duration-500" />
-          </Link>
+            {quote}
+          </p>
+          <p
+            className={`${sacramento.className} text-zinc-600 text-center mt-3`}
+          >
+            -{author}
+          </p>
+
+          <p
+            className={`${bebas_neus.className} ml-6 mt-5 text-2xl text-zinc-600 uppercase tracking-[0.5em] text-center`}
+          >
+            {user?.user_metadata.display_name || user?.user_metadata.full_name}
+          </p>
         </div>
       </div>
+
+      <Link
+        href={`/diary/${getLocalYear()}`}
+        className="absolute bottom-8 ml-6 text-xl font-medium text-zinc-500 hover:text-zinc-700 transition duration-300 uppercase tracking-[0.4em]"
+      >
+        Open
+      </Link>
     </div>
   );
 }
