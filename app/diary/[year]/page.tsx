0e62519bf99377ee8b069bf502641b5673e78ca7
Koushik <programmer.koushik@gmail.com>
2024-08-23 21:43:34 +0530

    update

--- app/diary/[year]/page.tsx
+++ app/diary/[year]/page.tsx
@@ -1,51 +1,60 @@
 import AddEntryButton from "@/components/global/AddEntryButton";
-import { motivationalQuotes } from "@/constants/motivationalQuotes";
 import { getUser } from "@/data/User";
 import { NotebookPen } from "lucide-react";
-import { RiDoubleQuotesL } from "react-icons/ri";
-import { Kalam } from "next/font/google";
 import React from "react";
-
-const kalam = Kalam({ weight: "400", subsets: ["latin"] });
-
-// Function to get the current day of the year
-const getDayOfYear = (date: Date): number => {
-  const start = new Date(date.getFullYear(), 0, 0);
-  const diff = date.getTime() - start.getTime();
-  const oneDay = 1000 * 60 * 60 * 24;
-  const dayOfYear = Math.floor(diff / oneDay);
-  return dayOfYear;
-};
-
-// Function to get the quote for the current day
-const getQuoteForToday = () => {
-  const dayOfYear = getDayOfYear(new Date());
-  // Use the modulo operator to cycle through the quotes array
-  const quoteIndex = dayOfYear % motivationalQuotes.length;
-  return motivationalQuotes[quoteIndex];
-};
 
 export default async function DiarySpace() {
   const user = await getUser();
 
-  const { quote, author } = getQuoteForToday();
-
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="max-w-sm px-6 py-16 flex flex-col items-center justify-center gap-8 lg:mr-16">
-        <RiDoubleQuotesL className="w-12 h-12 text-gray-600" />
-        <p className={`text-xl text-gray-600 text-center ${kalam.className}`}>
-          {quote}
-        </p>
-        <p className={`text-xl text-gray-600 text-center ${kalam.className}`}>
-          {author}
-        </p>
+    <div className="flex flex-col mt-2 mx-4 lg:mx-16 p-8">
+      {/* Achievement Stats */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-12">
+        <div className="border border-zinc-300 rounded-xl px-6 py-8 text-center shadow-lg hover:scale-105 transform transition-transform duration-300">
+          <div className="text-5xl font-semibold text-zinc-600 mb-2">20</div>
+          <p className="font-bold text-zinc-600">Diary Entry</p>
+          <p className="text-zinc-500">Total diary entries</p>
+        </div>
+        <div className="border border-zinc-300 rounded-xl px-6 py-8 text-center shadow-lg hover:scale-105 transform transition-transform duration-300">
+          <div className="text-5xl font-semibold text-zinc-600 mb-2">20</div>
+          <p className="font-bold text-zinc-600">Time Capsule</p>
+          <p className="text-zinc-500">Total time capsules created</p>
+        </div>
+        <div className="border border-zinc-300 rounded-xl px-6 py-8 text-center shadow-lg hover:scale-105 transform transition-transform duration-300">
+          <div className="text-5xl font-semibold text-zinc-600 mb-2">20</div>
+          <p className="font-bold text-zinc-600">Private Memory Vault</p>
+          <p className="text-zinc-500">Total private memories created</p>
+        </div>
       </div>
 
-      <div className="absolute bottom-10 right-10">
+      {/* Weekly Strike */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-12 sm:mb-20 lg:mb-0">
+        <div className="border border-zinc-300 rounded-xl px-6 py-8 text-center shadow-lg hover:scale-105 transform transition-transform duration-300">
+          <p className="font-semibold text-zinc-600 mb-2">
+            Current Week Strike
+          </p>
+          <div className="text-5xl font-bold text-zinc-600">5</div>
+        </div>
+        <div className="border border-zinc-300 rounded-xl px-6 py-8 text-center shadow-lg hover:scale-105 transform transition-transform duration-300">
+          <p className="font-semibold text-zinc-600 mb-2">
+            Current Week Strike
+          </p>
+          <div className="text-5xl font-bold text-zinc-600">5</div>
+        </div>
+        <div className="border border-zinc-300 rounded-xl px-6 py-8 text-center shadow-lg hover:scale-105 transform transition-transform duration-300">
+          <p className="font-semibold text-zinc-600 mb-2">
+            Current Week Strike
+          </p>
+          <div className="text-5xl font-bold text-zinc-600">5</div>
+        </div>
+      </div>
+
+      {/* Add Entry Button */}
+      {/* Add Entry Button */}
+      <div className="fixed bottom-10 right-10 sm:bottom-5 sm:right-5">
         <AddEntryButton
           userId={user?.id!}
-          className="flex items-center justify-center gap-2 border border-zinc-400 hover:border-zinc-800 transition-colors rounded-full px-5 py-3"
+          className="flex items-center justify-center gap-2 border border-zinc-400 hover:border-zinc-800 transition-colors rounded-full px-5 py-3 bg-white"
         >
           <NotebookPen className="w-4 h-4" />
           <span>Add Today's Entry</span>
