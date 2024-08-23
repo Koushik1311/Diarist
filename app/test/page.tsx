d7ee2ef7dc7127d279ed96f6f87bb3ca42e048a6
Koushik <programmer.koushik@gmail.com>
2024-08-23 21:54:43 +0530

    error fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,40 +1,4 @@
-"use client";
-
-// import { fetchTimeCapsuleEntry } from "@/data/timeCapsule";
-import { DateTimePicker } from "@/components/time-capsule/DateTimePicker";
-import SaveAlert from "@/components/time-capsule/SaveAlert";
-import {
-  fetchIfNull,
-  fetchLockedTimeCapsule,
-} from "@/data/client/time-capsule";
-import { fetchTimeCapsuleEntry } from "@/data/server/timeCapsule";
-import { browserClient } from "@/utils/supabase/client";
-import { useEffect } from "react";
-
 export default async function Page() {
-  // const getRecord = async () => {
-  // const { data, error } = await fetchTimeCapsuleEntry(
-  //   "e34835eb-9fc7-496b-9976-d4ecbf944078"
-  // );
-
-  // if (error) console.log(error);
-
-  // console.log(data);
-  // };
-
-  useEffect(() => {
-    const getdata = async () => {
-      const { data, error } = await fetchIfNull();
-
-      if (error) {
-        console.log(error);
-      }
-      console.log(data);
-    };
-
-    getdata();
-  }, []);
-
   return (
     <div>
       <h1>hello</h1>
