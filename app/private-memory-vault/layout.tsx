2d1af1d435ef610f5f58661d79bda883b2aebca0
Koushik <programmer.koushik@gmail.com>
2024-08-24 08:44:01 +0530

    private memory vault layout

---
+++ app/private-memory-vault/layout.tsx
@@ -0,0 +1,33 @@
+import { fetchSubscription } from "@/data/server/subscription";
+import { getUser } from "@/data/User";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import React from "react";
+import { Toaster } from "sonner";
+
+export const metadata: Metadata = {
+  title: "Diarist - Private Memory Vault",
+};
+
+export default async function DiaryRootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getUser();
+  if (!user) {
+    return redirect("/login");
+  }
+  const subscriptionData = await fetchSubscription(user?.id!);
+
+  if (!subscriptionData) {
+    return redirect("/onboarding");
+  }
+
+  return (
+    <>
+      <Toaster position="top-right" duration={2000} />
+      {children}
+    </>
+  );
+}
