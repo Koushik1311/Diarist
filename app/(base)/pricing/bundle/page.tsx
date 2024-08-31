041b93bfe004120198c1768115f6e6bcb011e530
Koushik <programmer.koushik@gmail.com>
2024-08-31 16:46:22 +0530

    for first 1000 users give 20 entries free

--- app/(base)/pricing/bundle/page.tsx
+++ app/(base)/pricing/bundle/page.tsx
@@ -1,13 +1,26 @@
 import BundlePricing from "@/components/subscription/BundlePricing";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { getUser } from "@/data/User";
 import React from "react";
 
-export default async function BundlePricingPage() {
+export default async function BundlePricingPage({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
   const user = await getUser();
   const isUser = !!user;
 
   return (
-    <div className="flex items-center justify-center lg:h-[90vh]">
+    <div className="flex flex-col items-center justify-center lg:h-[90vh]">
+      {searchParams.message && (
+        <div className="mt-20">
+          <Alert className="border-violet-500">
+            <AlertTitle></AlertTitle>
+            <AlertDescription>{searchParams.message}</AlertDescription>
+          </Alert>
+        </div>
+      )}
       <BundlePricing isUser={isUser} />
     </div>
   );
