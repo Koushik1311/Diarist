e4977035c0febb3a3010a4589909a3f81a70581b
Koushik <programmer.koushik@gmail.com>
2024-09-02 17:36:57 +0530

    lifetime pricing upgrade

--- app/(base)/pricing/lifetime/page.tsx
+++ app/(base)/pricing/lifetime/page.tsx
@@ -1,4 +1,5 @@
 import LifetimeAccess from "@/components/subscription/LifetimeAccess";
+import LifetimeUpgrade from "@/components/subscription/LifetimeUpgrade";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { getUser } from "@/data/User";
 import React from "react";
@@ -9,10 +10,9 @@ export default async function LifetimePricingPage({
   searchParams: { message: string };
 }) {
   const user = await getUser();
-  const isUser = !!user;
 
   return (
-    <div className="flex items-center justify-center lg:h-[90vh]">
+    <div className="flex flex-col items-center justify-center lg:h-[90vh]">
       {searchParams.message && (
         <div className="mt-20">
           <Alert className="border-violet-500">
@@ -21,7 +21,7 @@ export default async function LifetimePricingPage({
           </Alert>
         </div>
       )}
-      <LifetimeAccess isUser={isUser} userId={user?.id!} />
+      {user ? <LifetimeUpgrade userId={user.id} /> : <LifetimeAccess />}
     </div>
   );
 }
